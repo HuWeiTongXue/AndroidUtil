456941d1e1848566fd382936915e00acee97373a
Amanda_Wang <wangchundl@163.com>
2018-02-03 14:17:29 +0800

    init

---
+++ myutil/src/main/java/com/monkey/myutil/LogUtil.java
@@ -0,0 +1,61 @@
+package com.monkey.myutil;
+
+import android.util.Log;
+
+/**
+ * Description: LogUtils日志工具类
+ * Author: Author
+ * Date: 2017-07-02
+ * Time: 17:31
+ */
+public class LogUtil {
+
+    // 是否需要打印bug，可以在application的onCreate函数里面初始化
+    public static boolean isDebug = true;
+
+    // 默认TAG
+    private static final String TAG = "TAG";
+
+    // 下面四个是默认tag的函数
+    public static void i(String msg) {
+        if (isDebug)
+            Log.i(TAG, msg);
+    }
+
+    public static void d(String msg) {
+        if (isDebug)
+            Log.d(TAG, msg);
+    }
+
+    public static void e(String msg) {
+        if (isDebug)
+            Log.e(TAG, msg);
+    }
+
+    public static void v(String msg) {
+        if (isDebug)
+            Log.v(TAG, msg);
+    }
+
+    // 下面是传入自定义tag的函数
+    public static void i(String tag, String msg) {
+        if (isDebug)
+            Log.i(tag, msg);
+    }
+
+    public static void d(String tag, String msg) {
+        if (isDebug)
+            Log.d(tag, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        if (isDebug)
+            Log.e(tag, msg);
+    }
+
+    public static void v(String tag, String msg) {
+        if (isDebug)
+            Log.v(tag, msg);
+    }
+}
+
